dd3af02a520654fc90daf188345cecdc5f47197b
Gabor Csardi <csardi.gabor@gmail.com>
2016-03-23 20:20:40 +0000

    Brush up DESCRIPTION, LICENSE, roxygenize

--- R/package.R
+++ R/package.R
@@ -1,8 +1,8 @@
 
 #' Create a Nice Web Page About Your GitHub Repositories
 #'
-#' This is a static website generator, specialized on creating a single page web
-  site about GitHub repositories, for an account or organization.
+#' This is a static website generator, specialized on creating a single
+#' page web site about GitHub repositories, for an account or organization.
 #'
 #' @docType package
 #' @name myghrepos
